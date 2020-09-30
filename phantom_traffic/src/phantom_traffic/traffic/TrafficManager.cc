0895eea8672ae3545a52cef5036ec71d0c543b68
Leon <l.f.mueller@student.tue.nl>
2020-09-30 22:16:31 +0200

    added blank traffic manager

---
+++ phantom_traffic/src/phantom_traffic/traffic/TrafficManager.cc
@@ -0,0 +1,30 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+//TODO take a look at PlatoonsPlusHumanTraffic and RingTrafficManager to insert traffic into the ring
+
+#include "TrafficManager.h"
+
+Define_Module(TrafficManager);
+
+void TrafficManager::initialize()
+{
+    // TODO - Generated method body
+}
+
+void TrafficManager::handleMessage(cMessage *msg)
+{
+    // TODO - Generated method body
+}
